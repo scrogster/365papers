a1c7e0c827e5040615274be8aa4588ff9035ed87
scrogster <scrogster@gmail.com>
2016-03-01 23:36:38 +1100

    Added code to extract publication year of each paper, and plot histo of same.

--- 365papers.R
+++ 365papers.R
@@ -2,10 +2,13 @@ library(gsheet)
 library(lubridate)
 library(dplyr)
 library(tidyr)
+library(stringr)
 library(ggplot2)
 
 
 papers<-gsheet2tbl('https://docs.google.com/spreadsheets/d/1Z_4x3w2SgpOdHQ028IiPEgbB6CSFC0YT3oUymE5UoBE/edit?usp=sharing')
+
+
 
 #add some sensible variable names
 names(papers)<-c("DateTime", "Tweeter", "Content", "PaperURL", "TweetLink")
@@ -14,7 +17,11 @@ papers$DateTime<-mdy_hm(papers$DateTime)
 ##
 tidy_papers<-papers %>%
               mutate(Hour=hour(DateTime), YearDay=yday(DateTime), 
-                     DOW=wday(DateTime,label=TRUE, abbr=TRUE), PaperNum=order(DateTime))
+                     DOW=wday(DateTime,label=TRUE, abbr=TRUE), PaperNum=order(DateTime),
+                     YearPub=as.numeric(str_extract(Content, "\\d{4}")))
+
+
+
 
 #Plot diurnal distribution of tweets
 ggplot(tidy_papers, aes(x=Hour))+
@@ -24,19 +31,26 @@ ggsave(file="diurnal-hist.png", width=4, height=4)
 
 #Plot distribution by day of week
 ggplot(tidy_papers, aes(x=DOW))+
-  geom_bar( fill="red", col="red", alpha=0.7) +
+  geom_bar( fill="blue", col="blue", alpha=0.7) +
   xlab("")+
-  theme_bw()
+  theme_bw()+
+  xlab("Day of week")
 ggsave(file="weekly-hist.png", width=4, height=4)
+
+#Plot distribution of publication years
+ggplot(tidy_papers, aes(x=YearPub))+
+  geom_histogram(binwidth = 1, fill="green", col="green", alpha=0.7) +
+  theme_bw()+
+  xlab("Year published")
+ggsave(file="yearpub-hist.png", width=4, height=4)
 
 #Plot cumulative sum vs time
 ggplot(tidy_papers, aes(x=YearDay, y=PaperNum))+
-  geom_step(col="red")+
-  xlim(c(1, 365))+
-  ylim(c(1, 365))+
+  geom_step(col="purple")+
+  xlim(c(1, 100))+
+  ylim(c(1, 100))+
   xlab("Day of Year")+
   ylab("Cumulative papers")+
-  ggtitle("365 papers")+
   geom_abline(slope=1, intercept=0, col="gray", lty=2)+
   theme_bw()
 ggsave(file="cumulative.png", width=4, height=4)
